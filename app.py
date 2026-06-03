328ed57dd29cb96bfa8232df6776156076d93b9e
jwmister21 <100645292+jwmister21@users.noreply.github.com>
2026-06-03 11:16:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1192,6 +1192,5 @@ def atualizar_dados_cliente(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
