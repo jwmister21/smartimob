0840ee520b0318b2fbbeb2001a82b61811e7200d
jwmister21 <100645292+jwmister21@users.noreply.github.com>
2026-06-03 17:13:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -140,14 +140,14 @@ def init_db():
         )
     """)
     
-    cursor.execute("PRAGMA table_info(clientes)")
-       colunas = [c[1] for c in cursor.fetchall()]
+   cursor.execute("PRAGMA table_info(clientes)")
+    colunas = [c[1] for c in cursor.fetchall()]
 
-         if "data_visita" not in colunas:
-            cursor.execute("""
-        ALTER TABLE clientes
-        ADD COLUMN data_visita TEXT
-    """)
+    if "data_visita" not in colunas:
+        cursor.execute("""
+            ALTER TABLE clientes
+            ADD COLUMN data_visita TEXT
+        """)
     conn.commit()
     conn.close()
     print("Banco de dados inicializado com todas as tabelas!")
