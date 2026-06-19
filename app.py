ceb6d4ace32be48f5801967cb84b70a317432ad9
jwmister21 <100645292+jwmister21@users.noreply.github.com>
2026-06-19 15:32:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2325,7 +2325,7 @@ def login():
             session["is_admin"] = usuario['is_admin']
             session["user_email"] = usuario['email']
             conn.close()
-            return redirect('/')
+            return redirect('/index')
 
         conn.close()
         return render_template("login.html", erro="E-mail ou senha incorretos.")
