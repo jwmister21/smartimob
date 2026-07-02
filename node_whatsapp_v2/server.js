bf38eb1e777d62594b10d0012f4d80d751b07b6c
jwmister21 <100645292+jwmister21@users.noreply.github.com>
2026-07-01 22:49:59 -0300

    Update server.js

--- node_whatsapp_v2/server.js
+++ node_whatsapp_v2/server.js
@@ -1,1 +1,153 @@
+const express = require("express");
+const cors = require("cors");
+const wppconnect = require("@wppconnect-team/wppconnect");
 
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+
+
+let client = null;
+
+let qrCode = null;
+
+let status = "disconnected";
+
+
+
+
+
+wppconnect.create({
+
+    session:"smartzen",
+
+    catchQR:(base64Qr)=>{
+
+        console.log("QR RECEBIDO");
+
+        qrCode = base64Qr;
+
+        status="waiting";
+
+    },
+
+
+    statusFind:(statusSession)=>{
+
+        console.log(
+        "STATUS:",
+        statusSession
+        );
+
+
+        if(
+        statusSession==="isLogged"
+        ){
+
+            status="connected";
+
+        }
+
+
+    }
+
+
+})
+.then((c)=>{
+
+
+client=c;
+
+
+});
+
+
+
+
+
+
+app.get(
+"/status",
+(req,res)=>{
+
+
+res.json({
+
+status:status
+
+});
+
+
+});
+
+
+
+
+
+
+app.get(
+"/qr",
+(req,res)=>{
+
+
+res.json({
+
+qr:qrCode,
+
+status:status
+
+});
+
+
+});
+
+
+
+
+
+
+
+app.post(
+"/disconnect",
+async(req,res)=>{
+
+
+if(client){
+
+await client.logout();
+
+}
+
+
+status="disconnected";
+
+
+res.json({
+
+success:true
+
+});
+
+
+});
+
+
+
+
+
+
+
+app.listen(
+3001,
+()=>{
+
+console.log(
+"WhatsApp V2 rodando porta 3001"
+);
+
+});
