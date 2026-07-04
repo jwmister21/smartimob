f7ae7d6ca4089a9ab97ef68f71e47f30f14fc174
jwmister21 <100645292+jwmister21@users.noreply.github.com>
2026-07-04 12:51:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import sqlite3
 import secrets
 import requests 
-import mysql.connector
 from datetime import datetime, timedelta
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 from werkzeug.utils import secure_filename
