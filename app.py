02f7142ecd0c36343033421b1f51ce4e33aa2211
jwmister21 <100645292+jwmister21@users.noreply.github.com>
2026-07-08 18:29:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2531,68 +2531,6 @@ def dashboard_ceo():
 
     return render_template("ceo/dashboard_ceo.html")
      
-@app.route('/site/<subdominio>')
-def exibir_site(subdominio):
-
-    conn = sqlite3.connect('/data/imobiliaria.db')
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-
-    empresa = cursor.execute(
-        """
-        SELECT *
-        FROM configuracoes_site
-        WHERE subdominio = ?
-        """,
-        (subdominio,)
-    ).fetchone()
-
-    if not empresa:
-        conn.close()
-        return "Site não encontrado", 404
-
-    rows = cursor.execute(
-        """
-        SELECT *
-        FROM imoveis
-        WHERE empresa_id = ?
-        ORDER BY id DESC
-        """,
-        (empresa['empresa_id'],)
-    ).fetchall()
-
-    imoveis = []
-
-    for row in rows:
-
-        imovel = dict(row)
-
-        fotos = cursor.execute(
-            """
-            SELECT nome_arquivo
-            FROM fotos_imoveis
-            WHERE imovel_id = ?
-            ORDER BY id ASC
-            """,
-            (imovel["id"],)
-        ).fetchall()
-
-        imovel["fotos"] = [
-            foto["nome_arquivo"]
-            for foto in fotos
-        ]
-
-        imoveis.append(imovel)
-
-    conn.close()
-
-    return render_template(
-        "template_site.html",
-        empresa=empresa,
-        imoveis=imoveis
-    )
-
-
 
 
 @app.route("/excluir-foto/<int:foto_id>")
@@ -5983,6 +5921,7 @@ def informa_imovel(imovel_id):
 
 
 
+ 
 
 
     # ==========================
